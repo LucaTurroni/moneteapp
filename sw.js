51e4fffc9a7ee66e07df1cacf25fb8b303016931
LucaTurroni <luca.turroni85@gmail.com>
2026-04-09 18:52:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'cercamoneta-v1.0.0.6';
+const CACHE_VERSION = 'cercamoneta-v1.0.0.7';
 const ASSETS = ['./', './index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_VERSION).then(c => c.addAll(ASSETS)));
